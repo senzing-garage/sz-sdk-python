9e9a1dbe01d3d9f604cffee16f7a82865ca3ef96
docktermj <michael@dockter.com>
2024-07-26 17:15:25 -0400

    #30 Update exported variables

--- src/senzing_abstract/__init__.py
+++ src/senzing_abstract/__init__.py
@@ -1,9 +1,21 @@
+from .constants import (
+    SZ_INITIALIZE_WITH_DEFAULT_CONFIGURATION,
+    SZ_NO_ATTRIBUTES,
+    SZ_NO_AVOIDANCES,
+    SZ_NO_FLAGS,
+    SZ_NO_LOGGING,
+    SZ_NO_REQUIRED_DATASOURCES,
+    SZ_NO_SEARCH_PROFILE,
+    SZ_VERBOSE_LOGGING,
+    SZ_WITHOUT_INFO,
+)
 from .szconfig_abstract import SzConfigAbstract
 from .szconfigmanager_abstract import SzConfigManagerAbstract
 from .szdiagnostic_abstract import SzDiagnosticAbstract
 from .szengine_abstract import SzEngineAbstract
 from .szengineflags import SzEngineFlags
 from .szerror import (
+    ENGINE_EXCEPTION_MAP,
     SzBadInputError,
     SzConfigurationError,
     SzDatabaseConnectionLostError,
@@ -21,6 +33,16 @@ from .szerror import (
 from .szproduct_abstract import SzProductAbstract
 
 __all__ = [
+    "ENGINE_EXCEPTION_MAP",
+    "SZ_INITIALIZE_WITH_DEFAULT_CONFIGURATION",
+    "SZ_NO_ATTRIBUTES",
+    "SZ_NO_AVOIDANCES",
+    "SZ_NO_FLAGS",
+    "SZ_NO_LOGGING",
+    "SZ_NO_REQUIRED_DATASOURCES",
+    "SZ_NO_SEARCH_PROFILE",
+    "SZ_VERBOSE_LOGGING",
+    "SZ_WITHOUT_INFO",
     "SzBadInputError",
     "SzConfigAbstract",
     "SzConfigManagerAbstract",
