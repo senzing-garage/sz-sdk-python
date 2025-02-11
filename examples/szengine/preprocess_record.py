423039a7ac4777987043e533b18228e608d4e9f1
docktermj <michael@dockter.com>
2025-02-11 10:12:24 -0500

    #181 Savepoint

--- examples/szengine/preprocess_record.py
+++ examples/szengine/preprocess_record.py
@@ -1,24 +1,26 @@
 #! /usr/bin/env python3
+import json
+
 from senzing import SzEngineFlags, SzError
 
 from . import get_sz_abstract_factory
 
 FLAGS = SzEngineFlags.SZ_RECORD_DEFAULT_FLAGS
-RECORD_DEFINITION = (
-    "{"
-    '"RECORD_TYPE": "PERSON",'
-    '"PRIMARY_NAME_LAST": "Smith",'
-    '"PRIMARY_NAME_FIRST": "Robert",'
-    '"DATE_OF_BIRTH": "12/11/1978",'
-    '"ADDR_TYPE": "MAILING",'
-    '"ADDR_LINE1": "123 Main Street, Las Vegas NV 89132",'
-    '"PHONE_TYPE": "HOME",'
-    '"PHONE_NUMBER": "702-919-1300",'
-    '"EMAIL_ADDRESS": "bsmith@work.com",'
-    '"DATE": "1/2/18",'
-    '"STATUS": "Active",'
-    '"AMOUNT": "100"'
-    "}"
+RECORD_DEFINITION = json.dumps(
+    {
+        "RECORD_TYPE": "PERSON",
+        "PRIMARY_NAME_LAST": "Smith",
+        "PRIMARY_NAME_FIRST": "Robert",
+        "DATE_OF_BIRTH": "12/11/1978",
+        "ADDR_TYPE": "MAILING",
+        "ADDR_LINE1": "123 Main Street, Las Vegas NV 89132",
+        "PHONE_TYPE": "HOME",
+        "PHONE_NUMBER": "702-919-1300",
+        "EMAIL_ADDRESS": "bsmith@work.com",
+        "DATE": "1/2/18",
+        "STATUS": "Active",
+        "AMOUNT": "100",
+    }
 )
 try:
     sz_abstract_factory = get_sz_abstract_factory()
