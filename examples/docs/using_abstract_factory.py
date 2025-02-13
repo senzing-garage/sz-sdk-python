2395d619c0d24e291d1ebf68349e5d37c12c7c35
docktermj <michael@dockter.com>
2025-02-13 17:58:42 -0500

    #181 Fix action errors

--- examples/docs/using_abstract_factory.py
+++ examples/docs/using_abstract_factory.py
@@ -2,6 +2,7 @@ from senzing import SzAbstractFactory, SzEngineFlags
 
 
 def perform_senzing_search(sz_abstract_factory: SzAbstractFactory, attributes: str) -> str:
+    """Example Senzing search."""
     sz_engine = sz_abstract_factory.create_engine()
     flags = SzEngineFlags.SZ_SEARCH_BY_ATTRIBUTES_DEFAULT_FLAGS
     search_profile = ""
--- examples/docs/using_sz_engine.py
+++ examples/docs/using_sz_engine.py
@@ -2,6 +2,7 @@ from senzing import SzEngine, SzEngineFlags
 
 
 def perform_senzing_search(sz_engine: SzEngine, attributes: str) -> str:
+    """Example Senzing search."""
     flags = SzEngineFlags.SZ_SEARCH_BY_ATTRIBUTES_DEFAULT_FLAGS
     search_profile = ""
     return sz_engine.search_by_attributes(attributes, flags, search_profile)
