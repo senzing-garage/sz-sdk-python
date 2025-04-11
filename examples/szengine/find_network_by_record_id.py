ecba6709e52840d0be609b514f86a7062075e262
docktermj <michael@dockter.com>
2025-04-11 11:54:40 -0400

    #206 Fix variables

--- examples/szengine/find_network_by_record_id.py
+++ examples/szengine/find_network_by_record_id.py
@@ -8,7 +8,7 @@ try:
     MAX_DEGREES = 2
     MAX_ENTITIES = 10
     record_list = [("CUSTOMERS", "1001"), ("CUSTOMERS", "1009")]
-    RECORD = sz_engine.find_network_by_record_id(record_list, MAX_DEGREES, BUILD_OUT_DEGREES, MAX_ENTITIES, flags)
-    print(f"\n{RECORD}\n")
+    RESULT = sz_engine.find_network_by_record_id(record_list, MAX_DEGREES, BUILD_OUT_DEGREES, MAX_ENTITIES, flags)
+    print(f"\n{RESULT}\n")
 except SzError as err:
     print(f"\nERROR: {err}\n")
