e8aa81efba4fb7b577b5b66de313a263c421d49b
Ant <ant@senzing.com>
2025-04-11 14:37:43 -0400

    #206 - Example change

--- examples/szconfig/delete_data_source.py
+++ examples/szconfig/delete_data_source.py
@@ -5,7 +5,6 @@ from . import sz_configmanager
 try:
     DATA_SOURCE_CODE = "TEST"
     sz_config = sz_configmanager.create_config_from_template()
-    RESULT = sz_config.delete_data_source(DATA_SOURCE_CODE)
-    print(f"\n{RESULT}\n")
+    _ = sz_config.delete_data_source(DATA_SOURCE_CODE)
 except SzError as err:
     print(f"\nERROR: {err}\n")
